9e9488eac923c1ef0a04400b9bd6c010477fc371
Gody <250132528+matecg@users.noreply.github.com>
2026-02-26 15:02:19 -0300

    Add function to digits, clear, and equal buttons

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
+const displayPara = document.querySelector('.calc-display');
+const digitBtns = document.querySelectorAll('.digit');
+const operatorBtns = document.querySelectorAll('.operator');
+const clearBtn = document.querySelector('.clear');
+const equalBtn = document.querySelector('.equal');
+let operand1 = '', operand2 = '', operator = '';
+
+digitBtns.forEach((btn) => btn.addEventListener('click', onDigitClicked));
+operatorBtns.forEach((btn) => btn.addEventListener('click', onOperatorClicked));
+clearBtn.addEventListener('click', onClearClicked);
+equalBtn.addEventListener('click', () => {
+    if (!operand1 || !operand2 || !operator) {
+        setCleanState();
+        return;
+    }
+    const result = operate(operator, operand1, operand2);
+    setCleanState();
+    displayPara.textContent = result.toString();
+})
+
+setCleanState();
+
 function operate(operator, operand1, operand2) {
     switch (operator) {
         case '+':
@@ -18,9 +40,38 @@ function operate(operator, operand1, operand2) {
     }
 }
 
-console.log(operate("+", "3", "-5"));
-console.log(operate("-", "3", "-5"));
-console.log(operate("*", "3", "-5"));
-console.log(operate("/", "3", "-5"));
-console.log(operate("+", 3, "-5"));
-console.log(operate("+", 3,-5));
+function onDigitClicked(e) {
+    const digit = e.target.textContent;
+    if (!operand1 && digit === '0') return;
+    if (!operand1 || !operator) {
+        operand1 += digit;
+        displayPara.textContent = operand1;
+        return;
+    }
+    operand2 += digit;
+    displayPara.textContent += digit;
+}
+
+function onOperatorClicked(e) {
+    if (!operand1 || operand2) return; // Maybe call operate if operand2 exists.
+
+    const op = e.target.textContent;
+    if (operator) {
+        const current = displayPara.textContent.split('');
+        current.pop();
+        displayPara.textContent = current.join('');
+    }
+    operator = op;
+    displayPara.textContent += op;
+}
+
+function setCleanState(){
+    displayPara.textContent = '0';
+    operand1 = '';
+    operand2 = '';
+    operator = '';
+}
+
+function onClearClicked() {
+    setCleanState();
+}
